2ef439f4eb76ca7c3f0b356e39ebf9eba748b5ec
Pieterjan van Dijk <Pieterjan.v.dijk@gmail.com>
2019-05-02 16:29:32 +0200

    |

---
+++ ice.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Icecream :)</title>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="ice.css">
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+  		<div class="container">
+    		<div class="navbar-header">
+      			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+	        		<span class="icon-bar"></span>
+	        		<span class="icon-bar"></span>
+	        		<span class="icon-bar"></span>                        
+      			</button>
+      			<a id='link' class="navbar-brand" href="#myPage">Logo</a>
+    		</div>
+    		<div class="collapse navbar-collapse" id="myNavbar">
+      			<ul class="nav navbar-nav navbar-right">
+			        <li><a id='link' href="#about">ABOUT</a></li>
+			        <li><a id='link' href="#services">SERVICES</a></li>
+			        <li><a id='link' href="#portfolio">PORTFOLIO</a></li>
+			        <li><a id='link' href="#pricing">PRICING</a></li>
+			        <li><a id='link' href="#contact">CONTACT</a></li>
+      			</ul>
+    		</div>	
+  		</div>
+	</nav>
+
+	<div id='header' id='services'>
+		<img src='img/logo.png' alt='logo'>
+		<h1>logo text</h1>
+		<p>descritpion here</p>
+	</div>
+
+	<div id="showgrid">
+	  	<div class="row">
+		    <div class="column"><img src='delete/1.jpg' alt='pic'></div>
+		    <div class="column"><img src='delete/2.jpg' alt='pic'></div>
+		    <div class="column"><img src='delete/3.jpg' alt='pic'></div>
+	  	</div>
+	  	<div class="row">
+		    <div class="column"><img src='delete/2.jpg' alt='pic'></div>
+		    <div class="column"><img src='delete/3.jpg' alt='pic'></div>
+		    <div class="column"><img src='delete/1.jpg' alt='pic'></div>
+	  	</div>
+	  	<div class="row">
+		    <div class="column"><img src='delete/3.jpg' alt='pic'></div>
+		    <div class="column"><img src='delete/2.jpg' alt='pic'></div>
+		    <div class="column"><img src='delete/1.jpg' alt='pic'></div>
+	  	</div>
+	</div>
+
+
+
+	<script type="text/javascript" src='ice.js'></script>
+</body>
+</html>
